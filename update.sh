5e7d65fb4e1873f36f1429aee0453f233eccfda5
Jesse <79180615+jesse-public@users.noreply.github.com>
2025-11-26 00:11:57 -0800

    feat: oidc

--- update.sh
+++ update.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
 docker compose down
+
+cd gasket-builder
+git pull
+sudo apt remove gasket-dkms
+sudo apt update
+sudo apt full-upgrade
+docker build --output . .
+sudo dpkg -i ./gasket-dkms_1.0-18_all.deb
+
+cd ..
 docker compose pull
 docker image prune -f
 COMPOSE_BAKE=true docker compose up --build -d
